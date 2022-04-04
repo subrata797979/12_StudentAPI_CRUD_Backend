18f07df10827734c5925f1f0d238a19848f9778e
subrata797979 <subrata797979@gmail.com>
2022-04-04 09:34:02 +0530

    all fixed

--- src/main/java/com/simple/StudentAPI/controller/StudentAPIController.java
+++ src/main/java/com/simple/StudentAPI/controller/StudentAPIController.java
@@ -1,12 +1,16 @@
 package com.simple.StudentAPI.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.simple.StudentAPI.entity.StudentAPI;
@@ -38,13 +42,26 @@ public class StudentAPIController {
 	}
 	
 	// POST Mapping
-	@PostMapping("/student/store")
+	@PostMapping("/student/add")
 	public String store(@RequestBody StudentAPI s) {
 		studentService.store(s);
-		return "Student Record Stored Successfully!";	// how to redirect to previous page after successful post or delete?
+		return "Student Record Added Successfully!";	// how to redirect to previous page after successful post or delete?
 	}
 	
 	// PUT Mapping
+	@PutMapping("/student/modify/{id}")
+	public String updateStudent(@RequestBody StudentAPI s, @PathVariable long id) {
+
+		Optional<StudentAPI> studentOptional = Optional.ofNullable(this.studentService.get(id));
+
+		if (!studentOptional.isPresent())
+			return "Student not found!";
+
+		s.setId(id);
+		studentService.store(s);
+
+		return "Student Record Modified Successfully!";
+	}
 	
 	// DELETE Mapping
 	@DeleteMapping("/student/delete/{id}")
